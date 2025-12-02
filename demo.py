7d0b1916d84a0c0fc9535ebcb4b1115ec47f10b7
Bishal Sarkar <bishalsarkar2736@gmail.com>
2025-12-02 16:52:36 +0600

    update acception

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 from us_visa.logger.logging import logging as custom_log
+from us_visa.exception.Custom_exception import USvisaException
+import sys
 
-custom_log.info("Welcome to our custom log")
+#custom_log.info("Welcome to our custom log")
+
+
+try:
+    a = 2/0
+
+except Exception as e:
+    raise USvisaException(e,sys)
